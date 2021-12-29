b00264ca156a2d6dac97c2ecc337e1d3d91b0346
Paul Hebert <paul@cloudfour.com>
2021-12-29 13:01:40 -0800

    Hook up renderer to form

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,3 +1,24 @@
 import { renderCanvas } from "../renderers/canvas/renderer.js";
+import { parse } from "../parser/parse.js";
 
-renderCanvas({ canvas: document.getElementById("canvas") });
+const form = document.querySelector("form");
+
+render();
+
+form.addEventListener("input", (e) => {
+  render();
+});
+
+function render() {
+  const iterations = document.querySelector('[name="iterations"]').value;
+  const seed = document.querySelector('[name="seed"]').value;
+
+  renderCanvas({
+    canvas: document.querySelector("canvas"),
+    commandString: parse({
+      seed,
+      rules: { A: "AB", B: "BA" },
+      iterations,
+    }),
+  });
+}
--- src/renderers/canvas/renderer.js
+++ src/renderers/canvas/renderer.js
@@ -2,8 +2,15 @@ export const renderCanvas = ({ commandString, canvas }) => {
   const ctx = canvas.getContext("2d");
   const position = { x: 0, y: 0 };
 
+  // Clear the canvas
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  // Start our path
   ctx.beginPath();
+  // Move to starting position
   ctx.moveTo(position.x, position.y);
+
+  // Handle commands
   commandString.split("").forEach((command) => {
     if (command === "A") {
       position.x += 10;
